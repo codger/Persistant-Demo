e9ea823f3414ab663d1dbe9e6109027391a443c1
John James <jjames@pacbell.net>
2020-07-18 12:12:34 -0700

    Initial Commit

---
+++ Persistant Demo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Persistant Demo
+//
+//  Created by John James Retina on 7/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Persistant Demo/Persistant_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Persistant_DemoApp.swift
+//  Persistant Demo
+//
+//  Created by John James Retina on 7/18/20.
+//
+
+import SwiftUI
+
+@main
+struct Persistant_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
